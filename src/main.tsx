8e8f902434daefe947fffd06d7cb171f952e1651
tereshonkov <tereshonkov.dima@gmail.com>
2026-07-08 20:58:36 +0300

    refactor: add eruda for debugging in production environment and update package.json

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 if (import.meta.env.DEV) {
   await import("./mockEnv");
 }
+if (import.meta.env.PROD) {
+  import("eruda").then((eruda) => eruda.default.init());
+}
 
 init();
 
@@ -18,12 +21,12 @@ const queryClient = new QueryClient({
       retry: 1,
     },
   },
-})
+});
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </StrictMode>
-)
+  </StrictMode>,
+);
